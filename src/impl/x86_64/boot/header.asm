e4bed81c42ef90bd6c245e4dff81cc0f108052b9
JacobAdams101 <jacob.adams2003@hotmail.co.uk>
2021-07-20 08:40:33 +0100

    Added the text into the readme file, the licsense at the top of most files and comments around the code

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,3 +1,20 @@
+;
+; Copyright © 2021 BIG SYSTEMS
+;
+; This file is part of BIG SYSTEMS OS.
+;
+; BIG SYSTEMS OS is free software: you can redistribute it and/or
+; modify it under the terms of the GNU Public License Version 2 as published
+; by the Free Software Foundation.
+;
+; BIG SYSTEMS OS is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+; Public License for more details.
+;
+; You should have received a copy of the GNU General Public License along
+; with this program.  If not, see <https://www.gnu.org/licenses/>.
+;
 section .multiboot_header
 header_start:
     ; This is the magic number Multi-Boot 2 will look for
