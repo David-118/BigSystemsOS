be5270e65606c5f4b4e7d4dafd95e1476551f42e
JacobAdams101 <jacob.adams2003@hotmail.co.uk>
2021-07-22 16:51:41 +0100

    Begun adding buttons to windows... still needs a lot of work but might help for you to see how the data structure for graphics will look

---
+++ src/kernel/src/button.h
@@ -0,0 +1,15 @@
+#include "stdbool.h"
+
+typedef struct
+{
+	unsigned int relativeX; //The x position relative to the origin of the parent component (e.g trueX = relativeX + myParentX)
+    unsigned int relativeY; //The y position relative to the origin of the parent component (e.g trueY = relativeY + myParentY)
+	unsigned int width; //The width of the button
+    unsigned int height; //The height of the button
+
+    bool isHightlighted; //Is the button highlighted (by the mouse hovering over it)
+    bool isPressed; //Is the button being pressed
+
+    char* TEXT[64]; //The text in the button
+
+}Button;
